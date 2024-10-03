c886d7ba361b8216ef87780f78589ef3fb9e160d
Benson Nderitu <156505675+Ben-the-Analyst@users.noreply.github.com>
2024-10-03 14:42:26 +0300

    Update _layout.tsx

--- app/(root)/(tabs)/_layout.tsx
+++ app/(root)/(tabs)/_layout.tsx
@@ -13,7 +13,7 @@ const TabIcon = ({
   <View
     className={`flex flex-row justify-center items-center rounded-full ${focused ? 'bg-general-300' : ''}`}>
     <View
-      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? 'bg-general-400' : ''}`}>
+      className={`rounded-full w-12 h-12 items-center justify-center ${focused ? 'bg-primary-500' : ''}`}>
       <Image
         source={source}
         tintColor="white"
